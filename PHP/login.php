60218eb3b282562420d4dc49c8f9e93bed06c7db
Karthick489 <91181314+Karthick489@users.noreply.github.com>
2023-04-09 23:50:16 +0530

    Add files via upload

---
+++ PHP/login.php
@@ -0,0 +1,43 @@
+<?php
+
+// Defining variables
+$db_host = "localhost";
+$db_pass = "";
+$db_user = "root";
+
+// Create connection
+$conn = new mysqli($db_host, $db_user, $db_pass, "Intern Task");
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+//Retrieve form data.
+$info = $_POST['info'];
+$creds = explode(":", base64_decode($info));
+
+
+// $email = "interntask@example.com";
+// $passwrd = "ask@2022";
+
+// Hashing Password
+$email = $creds[0];
+$passwrd = sha1((strrev($creds[1])));
+
+$sttmnt = $conn->prepare("SELECT id,email FROM users where email = ? AND passwrd = ? ");
+$sttmnt->bind_param("ss", $email, $passwrd);
+$sttmnt->execute();
+
+$result = $sttmnt->get_result();
+
+$row = $result->fetch_assoc();
+if ($row) {
+  try {
+    $result = json_encode($row);
+    echo $result;
+  } catch (Error $e) {
+    echo $e;
+  }
+} else {
+  echo "User not found";
+}
